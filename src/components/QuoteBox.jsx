319de2f504929c2d8e3dfb0aa9694ad458a672ea
Dimagnic <dimagnic@penguin>
2022-12-28 08:07:25 -0500

    Actualizacion del Proyecto

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -4,36 +4,36 @@ import quotes from '../quotes.json'
 import { useState } from 'react'
 console.log(quotes)
 const QuoteBox = () => {
+
     const random = Math.floor(Math.random() * quotes.length)
     let colorRandom = Math.floor(Math.random() * 16777215).toString(16);
+    
     const [index, setIndex] = useState(random)
-    const [color, setColor] = useState("C5CAE9")
+    const [color, setColor] = useState(colorRandom)
 
 
     //const index = 3
     const newRandom = Math.floor(Math.random() * quotes.length)
+    let newColorRandom = Math.floor(Math.random() * 16777215).toString(16);
     const change = () => {
         setIndex(newRandom)
-        setColor(colorRandom)
+        setColor(newColorRandom)
     }
     
-
-
-
     return (
-        <div className='Container' style={{background:`#${color}`}}>
+        <div className='Container' style={{ background: `#${color}` }}>
 
             <div className='Card'>
                 <div className='Information'>
-                    <i style={{color:`#${color}`}} className=" quote2 fa-solid fa-quote-left"></i>
-                    <p style={{color:`#${color}`}}>{quotes[index].quote}</p>
+                    <i style={{ color: `#${color}` }} className=" quote2 fa-solid fa-quote-left"></i>
+                    <p style={{ color: `#${color}` }}>{quotes[index].quote}</p>
 
                 </div>
                 <div className='container-author'>
-                    <h2 style={{color:`#${color}`}} className=''>{quotes[index].author}</h2>
+                    <h2 style={{ color: `#${color}` }} className=''>{quotes[index].author}</h2>
                 </div>
                 <div className='Arrow-Information'>
-                    <button style={{background:`#${color}`}} onClick={change} className='button'><i className=" arrow fa-solid fa-arrow-right"></i></button>
+                    <button style={{ background: `#${color}` }} onClick={change} className='button'><i className=" arrow fa-solid fa-arrow-right"></i></button>
                 </div>
             </div>
 
